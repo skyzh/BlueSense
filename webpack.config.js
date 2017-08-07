a4e06d6e9e544e32796360dcaec924cee557e5fd
SkyZH <iSkyZH@gmail.com>
2017-08-07 20:02:51 +0800

    exclude firebase function in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -93,7 +93,7 @@ module.exports = function makeWebpackConfig() {
       {
         test: /\.ts$/,
         loaders: ['awesome-typescript-loader?' + atlOptions, 'angular2-template-loader', '@angularclass/hmr-loader'],
-        exclude: [isTest ? /\.(e2e)\.ts$/ : /\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/]
+        exclude: [isTest ? /\.(e2e)\.ts$/ : /\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/, /functions\//]
       },
 
       // copy those assets to output
