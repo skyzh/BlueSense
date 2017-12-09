32011d65b036c547e4f3164b188a0bb36f40bc47
SkyZH <iSkyZH@gmail.com>
2017-12-09 12:07:53 +0800

    update rss url and rss title

--- functions/rss.js
+++ functions/rss.js
@@ -7,12 +7,12 @@ const debug = require('debug')('bluesense:rss*');
 let feed = new RSS({
   title: 'BlueSense Hourly Data',
   description: 'Hourly summurized data for BlueSense',
-  feed_url: 'https://us-central1-bluesense-9e31b.cloudfunctions.net/feed',
+  feed_url: 'https://bluesense.skyzh.xyz/feed',
   site_url: 'https://bluesense.skyzh.xyz',
 //image_url: 'http://example.com/icon.png',
 //docs: 'http://example.com/rss/docs.html',
-  managingEditor: 'Sky Zhang',
-  webMaster: 'Sky Zhang',
+  managingEditor: 'iskyzh@gmail.com (Sky Zhang)',
+  webMaster: 'iskyzh@gmail.com (Sky Zhang)',
   copyright: '2017 Sky Zhang',
   language: 'en',
 //categories: [],
@@ -21,19 +21,20 @@ let feed = new RSS({
 });
 
 const feed_item = (key, data) => {
+  let title = `Reported at ${moment(data.time * 1000).format('LLLL')} | 
+  Temperature: ${_.round(data.tc, 2)}°C, 
+  Humidity: ${_.round(data.hum, 2)}%, 
+  Pressure: ${_.round(data.pressure)} Pa, 
+  PM10: ${_.round(data.pm10, 2)} µg/m, 
+  PM2.5: ${_.round(data.pm25, 2)} µg/m`;
+
   feed.item({
-    title:  `Reported at ${moment(data.time * 1000).format('LLLL')}`,
-    description: `Reported at ${moment(data.time * 1000).format('LLLL')} | 
-    Temperature: ${_.round(data.tc, 2)}°C, 
-    Humidity: ${_.round(data.hum, 2)}%, 
-    Pressure: ${_.round(data.pressure)} Pa, 
-    PM10: ${_.round(data.pm10, 2)} µg/m, 
-    PM2.5: ${_.round(data.pm25, 2)} µg/m,    
-    `,
-    url: 'https://bluesense.skyzh.xyz', // link to the item
+    title:  title,
+    description: title,
+    url: `https://bluesense.skyzh.xyz/charts/hourly/?data=${key}`, // link to the item
     guid: key, // optional - defaults to url
 //  categories: ['Category 1','Category 2','Category 3','Category 4'], // optional - array of item categories
-    author: 'Sky Zhang', // optional - defaults to feed author property
+    author: 'iskyzh@gmail.com (Sky Zhang)', // optional - defaults to feed author property
     date: moment(data.time * 1000).format('LLLL'), // any format that js Date can parse.
   });
 };
