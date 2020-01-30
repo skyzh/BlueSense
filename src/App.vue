d1d79d689d424ea049eda2b7a8f87940f2a2b640
Alex Chi <iskyzh@gmail.com>
2020-01-30 12:50:56 +0800

    add logo

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="col-6">
+      <div class="embed-responsive embed-responsive-1by1">
+        <div
+          class="bs-logo-container embed-responsive-item d-flex flex-column align-item-center justify-content-center"
+        >
+          <h1 class="bs-logo-text">Sense</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+@import "~@/shadow.scss";
+</style>
