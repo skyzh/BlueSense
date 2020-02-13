e3d578f10fd5e93d787496bf849fbcc10bcbe2c2
Alex Chi <iskyzh@gmail.com>
2020-02-13 17:45:15 +0800

    add line and text to graph

--- src/chart/index.ts
+++ src/chart/index.ts
@@ -1,4 +1,5 @@
 import SenseCheckpoint from "../checkpoint"
+import { roundDigit } from "../utils"
 
 class Axis {
     minData: number
@@ -17,7 +18,7 @@ class Axis {
         const axis = new Axis
         axis.maxData = Math.max(...data)
         axis.minData = Math.min(...data)
-        const marginSize = (maxPos - minPos) * margin / 2
+        const marginSize = (maxPos - minPos) * margin
         maxPos -= marginSize
         minPos += marginSize
         axis.minPos = minPos
@@ -49,11 +50,21 @@ export function setupCanvas(canvas: HTMLCanvasElement) {
 const STROKE_COLOR = "#2196F3"
 const FILL_COLOR = "rgba(33,150,243,0.3)"
 
-function drawSeries(ctx: CanvasRenderingContext2D, offsetX: number, offsetY: number, width: number, height: number, data: number[], time: number[], field: string) {
+function truncateTime(t: number, step: number): number {
+    return Math.floor(t / step) * step
+}
+
+function drawSeries(ctx: CanvasRenderingContext2D, name: string, offsetX: number, offsetY: number, width: number, height: number, data: number[], time: number[], field: string) {
     const N = data.length
     const maxY = height + offsetY
-    const axisY = Axis.fromArray(data, maxY, offsetY, 0.1)
-    const axisX = Axis.fromArray(time, offsetX, offsetX + width, 0.1)
+    const yMargin = 0.1
+    const xMargin = 0.1
+    const axisY = Axis.fromArray(data, maxY, offsetY, yMargin)
+    const axisX = Axis.fromArray(time, offsetX, offsetX + width, xMargin)
+
+    // draw data
+    ctx.strokeStyle = STROKE_COLOR
+    ctx.setLineDash([])
     ctx.beginPath()
     let firstCall = true
     ctx.moveTo(axisX.posOf(axisX.minData), maxY)
@@ -67,22 +78,78 @@ function drawSeries(ctx: CanvasRenderingContext2D, offsetX: number, offsetY: num
             ctx.lineTo(x, y)
         }
     }
-    ctx.strokeStyle = STROKE_COLOR
     ctx.stroke()
     ctx.lineTo(axisX.posOf(axisX.maxData), maxY)
     ctx.lineTo(axisX.posOf(axisX.minData), maxY)
     ctx.lineTo(axisX.posOf(axisX.minData), axisX.posOf(data[0]))
     ctx.fillStyle = FILL_COLOR
     ctx.fill()
+
+    // draw vertical split line
+    firstCall = true
+    ctx.strokeStyle = "#cccccc"
+    ctx.setLineDash([8, 8])
+    let lstTimeChunk = 0
+    for (let i = 0; i < N; i++) {
+        const x = axisX.posOf(time[i])
+        const t = time[i]
+        const tChunk = truncateTime(t, 3600)
+        if (tChunk != lstTimeChunk) {
+            if (!firstCall) {
+                ctx.beginPath()
+                ctx.moveTo(x, maxY)
+                ctx.lineTo(x, axisY.maxPos)
+                ctx.stroke()
+            }
+            lstTimeChunk = tChunk
+            firstCall = false
+        }
+    }
+
+    // draw horizontal split line
+
+    ctx.beginPath()
+    ctx.moveTo(axisX.minPos, axisY.maxPos)
+    ctx.lineTo(axisX.maxPos, axisY.maxPos)
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.moveTo(axisX.minPos, axisY.minPos)
+    ctx.lineTo(axisX.maxPos, axisY.minPos)
+    ctx.stroke()
+
+    // draw text
+    ctx.fillStyle = "black"
+    ctx.textAlign = "start"
+    ctx.textBaseline = "bottom"
+    ctx.font = "14px Merriweather"
+    ctx.fillText(name, offsetX + width * xMargin, offsetY + 5)
+
+    ctx.textAlign = "end"
+    ctx.textBaseline = "middle"
+    ctx.fillText(`${roundDigit(axisY.maxData, 2)}`, axisX.minPos, axisY.maxPos)
+    ctx.fillText(`${roundDigit(axisY.minData, 2)}`, axisX.minPos, axisY.minPos)
+}
+
+function yPositionOf(id: number, n: number, height: number, margin: number): number {
+    const content = 1 - margin
+    const allPercent = content * n + margin * (n + 1)
+    return height / allPercent * (content * id + margin * (id + 1))
+}
+
+function splitHeight(n: number, height: number, margin: number): number {
+    const content = 1 - margin
+    const allPercent = content * n + margin * (n + 1)
+    return height / allPercent * content
 }
 
 export function drawCall(ctx: CanvasRenderingContext2D, width: number, height: number, data: SenseCheckpoint[]) {
     ctx.clearRect(0, 0, width, height)
-    const seriesHeight = height / 5
     const time = data.map(d => d.time)
-    drawSeries(ctx, 0, 0, width, seriesHeight, data.map(d => d.temp), time, "temp")
-    drawSeries(ctx, 0, seriesHeight, width, seriesHeight, data.map(d => d.hum), time, "hum")
-    drawSeries(ctx, 0, seriesHeight * 2, width, seriesHeight, data.map(d => d.pa), time, "pa")
-    drawSeries(ctx, 0, seriesHeight * 3, width, seriesHeight, data.map(d => d.pm25), time, "pm25")
-    drawSeries(ctx, 0, seriesHeight * 4, width, seriesHeight, data.map(d => d.pm10), time, "pm10")
+    const seriesHeight = splitHeight(5, height, 0.1)
+    drawSeries(ctx, "Temperature  °C", 0, yPositionOf(0, 5, height, 0.1), width, seriesHeight, data.map(d => d.temp), time, "temp")
+    drawSeries(ctx, "Humidity  %", 0, yPositionOf(1, 5, height, 0.1), width, seriesHeight, data.map(d => d.hum), time, "hum")
+    drawSeries(ctx, "Pressure  kPa", 0, yPositionOf(2, 5, height, 0.1), width, seriesHeight, data.map(d => d.pa / 1000), time, "pa")
+    drawSeries(ctx, "PM2.5  µg/m^3", 0, yPositionOf(3, 5, height, 0.1), width, seriesHeight, data.map(d => d.pm25), time, "pm25")
+    drawSeries(ctx, "PM10  µg/m^3", 0, yPositionOf(4, 5, height, 0.1), width, seriesHeight, data.map(d => d.pm10), time, "pm10")
 }
